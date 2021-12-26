dc4ab0cc0bc3a0d4b66097cd2268214e2fa70252
Juan_Giacobbe <jgiacobbe32@gmail.com>
2021-12-26 17:01:49 -0300

    finish first css version

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const url = 'https://pokeapi.co/api/v2/pokemon';
+// DOM SECTION
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.id');
+const result = document.querySelector('.result');
+const txt = document.querySelector('.name');
+const img = document.querySelector('.img');
+const hei = document.querySelector('.height');
+
+// FUNCTIONS
+
+function renderPokemon(name, image, height){
+    txt.innerHTML = name;
+    img.setAttribute('src', image);
+    hei.innerHTML = height;
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let id = input.value;
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then(data => {
+        let name = data.name;
+        let image = data.sprites.front_default;
+        let height = data.height;
+        renderPokemon(name, image, height);
+    })
 
 
-fetch(url)
-.then(res => res.json())
-.then(data => console.log(data.name))
-.catch(err => console.log('error'))
+
+    .catch(err => console.log('error'))
+
+
+
+})
+
+
